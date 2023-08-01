ffd980e8e5a2d2d1617c8ef936d467f35cf1bd82
miz <mizerablepi@gmail.com>
2023-08-01 14:44:59 +0530

    update configs

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: '/\.css$/i',
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: '/\.svg$/i',
+        loader: 'svg-inline-loader',
+      },
+      {
+        test: '/(\.png$)/i',
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
